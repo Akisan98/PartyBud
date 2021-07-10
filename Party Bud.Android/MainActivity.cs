9f9dc8962a10c37eb0c7cd41bfdef05f4549f3ce
Akisan <51326806+Akisan98@users.noreply.github.com>
2021-07-10 19:13:17 +0200

    Close to Done!

--- Party Bud.Android/MainActivity.cs
+++ Party Bud.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace Party_Bud.Droid
 {
-    [Activity(Label = "Party_Bud", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Party Bud", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- Party Bud/AssemblyInfo.cs
+++ Party Bud/AssemblyInfo.cs
@@ -1,5 +1,12 @@
+using Android.App;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: ExportFont("MouseMemoirs-Regular.ttf", Alias = "FunFont")]
+
+#if DEBUG
+[assembly: Application(Debuggable = true)]
+#else
+[assembly: Application(Debuggable=false)]
+#endif
