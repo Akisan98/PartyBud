64cc03890829ddd2fff604df11bf7cae86b0e1bc
Akisan <51326806+Akisan98@users.noreply.github.com>
2021-08-15 12:44:37 +0200

    Updated with Support for smaller screens

--- Party Bud/AssemblyInfo.cs
+++ Party Bud/AssemblyInfo.cs
@@ -1,12 +1,5 @@
-using Android.App;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: ExportFont("MouseMemoirs-Regular.ttf", Alias = "FunFont")]
-
-#if DEBUG
-[assembly: Application(Debuggable = true)]
-#else
-[assembly: Application(Debuggable=false)]
-#endif
